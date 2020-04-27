57c418f2666bc192f3d1985305d6f94bf2ec2cac
jlf <jlf.inbox@icloud.com>
2020-04-27 01:37:44 -0600

    python version of Woo

--- Programming Regular/Eddie_Woo_Zero_Zero/zero_power_zero.cpp
+++ Programming Regular/Eddie_Woo_Zero_Zero/zero_power_zero.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include <math.h>
+#include <iomanip>
+
 using namespace std;
 
+/*
+	JLF  -  April 27, 2020
+
+	We were watching Eddie Woo talk about zero^zero = 1 and the 
+	limit and we decided to write this little program to test that
+	idea for outselves.  What happens as lim x->0 x^x?
+*/
 
 // -------------------------------------------------------------------
 
@@ -20,7 +29,7 @@ float powerize( float powerThing ) {
 
 int main() {
 
-	float f1 = 0.1;
+	float f1 = 1.0;
 	int ct1 = 0;
 
 	cout << "\n\n*******************************\n\n";
@@ -29,7 +38,8 @@ int main() {
 
 	for ( ct1 = 0; ct1 < 15; ct1 ++ ) {
 	
-		cout << "\n" << f1 << "^" << f1 << " => " << powerize(f1);
+		cout << setprecision(4) << setw(10) << "\n" << f1 << "^" << f1 << " => ";
+		cout << setprecision(10) << setw(10) << powerize(f1);
 		f1 = f1 / 10;
 	} 
 	
---
+++ Programming Regular/Eddie_Woo_Zero_Zero/zero_power_zero.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+
+'''
+	JLF  -  April 27, 2020
+
+	We were watching Eddie Woo talk about zero^zero = 1 and the 
+	limit and we decided to write this little program to test that
+	idea for outselves.  What happens as lim x->0 x^x?
+'''
+
+
+def makeLine():
+	print()
+	print('something')
+	print()
+
+def main():
+	makeLine()
+
+main()
