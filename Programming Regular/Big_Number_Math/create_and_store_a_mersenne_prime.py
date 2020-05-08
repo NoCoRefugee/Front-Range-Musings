71bcb9b2735936269214d490ac8bf386ff263a15
jlf <jlf.inbox@icloud.com>
2020-05-08 07:10:02 -0600

    mersenne stuff

---
+++ Programming Regular/Big_Number_Math/create_and_store_a_mersenne_prime.py
@@ -0,0 +1,46 @@
+# This program should ask you for a number 'n', and then give you
+# the result of (2^n) - 1.  It should store this number in a file
+# for you, if you want it to.
+
+# jlf  -  20200507
+
+print("\n-------------------------------------------------------------------\n")
+
+# ask the user for an 'n' to use in computing the mersenne number
+inp1 = input("give me an exponent to work with and I will make you a mersenne number from it: ")
+
+# convert that input to an integer
+num1 = int(inp1)
+
+print()
+print("you gave me => ", num1 )
+#print("type for variable num1 is: ", type(num1))
+print()
+
+
+num2 = (2**num1) - 1
+print(f"(2^n) - 1 for that number is: \n{num2:,}")
+
+print()
+inp2 = input("Shall I store that number in a file for you? (y/n)")
+print()
+
+if inp2 == "y":
+	print(f"you said Yes")
+else:
+	print(f"you said No")
+
+
+fileName = "Mersenne_number_data/mersenne_number_2^" + inp1 + "-1"
+
+print("filename is => ", fileName)
+
+if inp2 == "y":
+	f = open(fileName, "w+" )
+	f.write(str(num2))
+	f.close()
+
+
+print("\n-------------------------------------------------------------------\n")
+
+
---
+++ Programming Regular/Small_Programs/big_numbers_python_practice.py
@@ -0,0 +1,30 @@
+# 	This is a little practice to get us used to big
+#	numbers in python, and how to show them.
+
+#	jlf  -  20200507
+
+# Let's see a big number:
+
+print('\n***************************************************\n');
+
+print('Here are some examples of how to show a big')
+print('number in python3')
+
+print('\n***************************************************\n');
+
+print("print(100**100)  => \n")
+print(100**100)
+
+print('\n***************************************************\n');
+
+print("print(f'{100**100:,}')  => \n")
+print(f'{100**100:,}')
+
+print('\n***************************************************\n');
+
+print("num1 = 100**100;\nprint(f'{num1:,}')  => \n")
+num1 = 100**100;
+print(f'{num1:,}')
+
+print('\n***************************************************\n');
+
