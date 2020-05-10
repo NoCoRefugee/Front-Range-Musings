00a56c8c7d3f24cd29e466fbf84f9f2d2b9d7c7f
hal7568m <jlf.hal7568m>
2020-05-09 23:17:47 -0600

    it's always fun adding something that doesn't work

--- Programming Regular/Eddie_Woo_Zero_Zero/zero_power_zero.py
+++ Programming Regular/Eddie_Woo_Zero_Zero/zero_power_zero.py
@@ -1,4 +1,11 @@
 #! /usr/bin/env python3
+import decimal
+
+#
+# this doesn't work.  figure it out.
+#
+
+
 
 '''
 	JLF  -  April 27, 2020
@@ -7,14 +14,40 @@
 	limit and we decided to write this little program to test that
 	idea for outselves.  What happens as lim x->0 x^x?
 '''
+# ------------------------------------------------------------------------------------------
 
 
 def makeLine():
 	print()
-	print('something')
+	print('*****************************************************************')
 	print()
+
+# ------------------------------------------------------------------------------------------
+
+def doIt(int1):
+
+	decimal.getcontext().prec = 100
+
+	return decimal.Decimal(int1)*decimal.Decimal(int1)
+
+# ------------------------------------------------------------------------------------------
 
 def main():
 	makeLine()
 
+	print("what is the Limit as x -> 0 of x^x?")
+
+	decimal.Decimal(num) = decimal.Decimal(0.1)
+	for x in range(20):
+		print(doIt(decimal.Decimal(num)))
+		print("decimal.Decimal(num) => ", decimal.Decimal(num))
+		decimal.Decimal(num) = decimal.Decimal(num) / 10
+
+	makeLine()
+
+# ------------------------------------------------------------------------------------------
+
 main()
+
+# ------------------------------------------------------------------------------------------
+
