f1bfc25365bd9f19806e5827dfdeb92d15dacff9
jlf <jlf.inbox@icloud.com>
2020-05-14 19:11:24 -0600

    c++ mersenne maker

---
+++ Programming Regular/Big_Number_Math/create_and_store_a_mersenne_prime.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+// -------------------------------------------------------------------
+
+int theNumber = 5;
+int arraySize = 0;
+
+// -------------------------------------------------------------------
+
+void initializeArray(int* arrayOne) {
+
+	arrayOne[0] = 2;
+
+} // end of initializeArray
+// -------------------------------------------------------------------
+
+int getArraySize(int* arrayOne) {
+
+	int i;
+	int sizeA = 0;
+	
+
+	return sizeA;
+
+} // end of getArraySize
+
+// -------------------------------------------------------------------
+
+void showArray(int* arrayOne) {
+	
+	int i;
+	
+	cout << endl;	
+
+	for ( i=arraySize; i--; i>=0 ) {
+		cout << arrayOne[i];
+	}	
+
+	cout << endl;	
+
+
+} // end of showArray
+
+// -------------------------------------------------------------------
+// -------------------------------------------------------------------
+// -------------------------------------------------------------------
+
+
+
+int main() {
+	
+	int mersenneN = theNumber;  // this will be the n of 2^n-1
+	int numberholder[20] = {0};
+
+	cout << "\n----------------------------------------------------\n";
+	
+	initializeArray(numberholder);
+
+//	cout << "\nThe array is => " << showArray(numberholder);
+
+	
+	cout << "\n----------------------------------------------------\n";
+
+
+} // end of main
+
+
+
