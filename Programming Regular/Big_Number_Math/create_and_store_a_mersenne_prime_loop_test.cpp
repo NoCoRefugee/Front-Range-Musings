08b905642ca514726edc8421c401f296ede19218
hal7568m <jlf.hal7568m>
2020-05-17 02:16:07 -0600

    stupid big numbers

--- Programming Regular/Big_Number_Math/create_and_store_a_mersenne_prime_loop_test.cpp
+++ Programming Regular/Big_Number_Math/create_and_store_a_mersenne_prime_loop_test.cpp
@@ -6,51 +6,68 @@ using namespace std;
 void showArray(int* array, int currentSize) {
 
 	cout << "\narray => ";
-	for ( int i=currentSize; i>=0; i-- ) {
-		cout << array[i] << ".";
-	} // end of for
-
-	cout << "\nsizeof returned => " << sizeof(array) << endl;
 	for ( int i=19; i>=0; i-- ) {
 		cout << array[i] << ".";
 	} // end of for
-
+/*
+	cout << "\narray => ";
+	for ( int i=currentSize; i>=0; i-- ) {
+		cout << array[i] << ".";
+	} // end of for
+*/
 } // end of showArray
 
 // -------------------------------------------------------------------------------
+
+void multiplyArrayByTwo(int* array, int currentSize) {
+
+	
+	int carryOver = 0;
+	int numberN = 5;
+	int i = 0;
+
+	for ( i=0; i<=currentSize; i++ ) {
+		array[i] = array[i] + carryOver;
+		array[i] = array[i] * 2;
+		cout << "\ncarryOver => " << carryOver;
+		if ( array[i] > 9 ) {
+			cout << "\ngreater than 9";
+			carryOver = 1;
+			array[i+1] = array[i+1] + carryOver;
+			array[i+5] = 8;
+			array[i] = array[i]%10;
+			if ( i > currentSize ) { currentSize++; } 
+		} // end of if
+		i++;
+
+		cout << "\ncurrentSize => " << currentSize;
+		cout << "\ncarryOver => " << carryOver << endl;
+		
+	} // end of for 
+
+} // end of multiplyArrayByTwo
 
 // -------------------------------------------------------------------------------
 
 int main() {
 
-	int carryOver = 0;
+
 	int array[20] = {0};
 	int currentSize = 1;
-	int numberN = 5;
-	int i = 0;
 
 	cout << "\n----------------------------------------\n";
 
-	array[0] = 2;
 	showArray(array, currentSize);
-// you need to make a seperate "multiply once by 2" function
-	while ( i<=numberN ) {
-		cout << "\nIn the loop";
-		array[i] = array[i] + carryOver;
-		array[i] = array[i] * 2;
-		cout << "\n=> " << array[0];
-		if ( array[i] > 9 ) {
-			carryOver = 1;
-			array[i] = array[i]%10;
-			i++;
-		} // end of if
-		if ( i > currentSize-1 ) {
-			currentSize++;
-		} // end of other if
-	} // end of while 
 
-	showArray(array, currentSize);
-	
+	array[0] = 2;
+
+	cout << "\n------------------------";
+	for ( int i=0; i<9; i++ ) {
+		showArray(array, currentSize);
+		multiplyArrayByTwo(array, currentSize);
+	}
+	cout << "\n------------------------";
+
 
 	cout << "\n----------------------------------------\n";
 
